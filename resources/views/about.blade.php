cff3538ba2fde8f0945566c11f009cea485c0084
Fraaiz <fraaiz130@gmail.com>
2020-07-30 18:39:36 +0200

    :sparkles: added text in treatments, added picture on home screen, added about, added foto at pricelist

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@inject('treatmentService', 'App\Services\TreatmentService')
+
+@section('content')
+<h1 class="text-center text-black uppercase text-4xl mt-8" >Over mij</h1>
+
+<h1 class="text-left text-grey text-sm mt-8" >mijn naam is nikki Brouwer en ik ben 20 jaar oud</h1>
+
+
+@endsection
+
+
+
+{{--    aanmaken dat dit een .blade bestand is en dat er een foto rechts van text komt doormiddelvan object-contain spul--}}
