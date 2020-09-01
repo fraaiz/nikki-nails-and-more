3ea309ca5b6eaeba2580ac94bbdaa91915012a97
Fraaiz <fraaiz130@gmail.com>
2020-09-01 18:09:41 +0200

    margin changed for smaller devices

--- resources/views/commingsoon.blade.php
+++ resources/views/commingsoon.blade.php
@@ -6,12 +6,8 @@
 
 
 <div>
-    <h1 class=" mx-64 mt-40 bg-red-300 shadow-lg rounded px-2 pt-6 pb-8  text-center text-black capitalize text-4xl " >Comming soon! </h1>
-</div>
-
-{{--     <h1 class="bg-red-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">comming soon </h1>--}}
-
-
+    <h1 class="  mx-4 sm:mx-64 mt-40 bg-red-300 shadow-lg rounded px-2 pt-6 pb-8  text-center text-black capitalize text-4xl  " >Comming soon!
+    </h1>
 
 
 
