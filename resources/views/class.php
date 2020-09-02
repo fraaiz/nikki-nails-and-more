c83146c8f06c4e330a3e35b3cc785b851a757ad7
Mike Brouwer <mike@brouwer.xyz>
2020-09-02 10:38:55 +0200

    :recycle: Small cleanup + fix for font

--- resources/views/class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class standaard {
-    font-size: 60px;
-    line-height: 62px;
-    font-family: 'Roboto', sans-serif ;
-
-
-}
--- resources/views/pricelist.blade.php
+++ resources/views/pricelist.blade.php
@@ -1,17 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-
-
-
-
-
-
-        <div>
-    <img class="mx-auto mt-10 my-10 w-1/3 h-full object-contain" src="/media/pricelist.png"
-        </div>
-
-
-
-
+    <div>
+        <img class="mx-auto mt-10 my-10 w-1/3 h-full object-contain" src="/media/pricelist.png"
+    </div>
 @endsection
