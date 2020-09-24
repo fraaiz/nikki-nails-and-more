7a708b7c906d6a1491372e5aa34dc1629994c314
Mike Brouwer <mike@brouwer.xyz>
2020-09-25 00:17:14 +0200

    :sparkels: Added routes staticly instead from the database

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,13 @@ use Illuminate\Support\Facades\Auth;
 //     Route::get($navigationItem->url, $navigationItem->action)->name($navigationItem->route_name);
 // }
 
+Route::get('/', 'HomeController@view')->name('home');
+Route::get('/treatments', 'TreatmentsController@view')->name('treatments');
+Route::get('/pricelist', 'PriceListController@view')->name('pricelist');
+Route::get('/reservation', 'ReservationController@view')->name('reservation');
+Route::get('/aboutme', 'AbouteMeController@view')->name('about');
+Route::get('/testpage', 'TestPageController@view')->name('testpage');
+
 Route::group(['prefix' => 'admin', 'as' => 'admin::', 'namespace' => 'Admin', 'middleware' => 'auth'], static function () {
         // All routes inside this group require a signed in user!
         Route::get('/', 'DashboardController@index')->name('dashboard');
